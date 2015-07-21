f01b585ccca8cb25a58d7411bd1919eab36fdfe7
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-21 11:38:19 -0400

    Board defaults to a 3x3 nil matrix

--- tictactoe/board.rb
+++ tictactoe/board.rb
@@ -1,6 +1,16 @@
 module TicTacToe
   class Board
-    def initialize
+    attr_accessor :cells
+
+    def initialize(w=3,h=3)
+      @cells = Array.new(w) { Array.new(h) { nil }} # w x h array of arrays
+    end
+
+    def width
+      @cells.length
+    end
+    def height
+      @cells[0].length
     end
   end
 end
--- tictactoe/spec/tictactoe_spec.rb
+++ tictactoe/spec/tictactoe_spec.rb
@@ -2,7 +2,18 @@ require_relative '../board'
 
 module TicTacToe
   describe Board do
-    subject(:board) { Board.new }
+    context "default board" do
+      subject(:board) { Board.new }
 
+      it "is empty" do
+        expect(board.cells).to eq([[nil,nil,nil],
+                                   [nil,nil,nil],
+                                   [nil,nil,nil]])
+      end
+      it "is a 3x3" do
+        expect(board.width).to eq(3)
+        expect(board.height).to eq(3)
+      end
+    end
   end
 end
