688416cc37990143df08fb5e32750e5714715045
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-21 10:00:09 -0400

    Finished today's kata

---
+++ katas/spec/coin_changer_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../coin_changer.rb'
+
+describe "make_change" do
+  let(:coin_changer) { CoinChanger.new }
+
+  context "where I expect only the same coin" do
+    it "works for quarters!" do
+      expect(coin_changer.make_change(75)).to eq([25,25,25])
+    end
+
+    it "works for dimes!" do
+      expect(coin_changer.make_change(20)).to eq([10,10])
+    end
+
+    it "works for nickels!" do
+      expect(coin_changer.make_change(5)).to eq([5])
+    end
+
+    it "works for pennies!" do
+      expect(coin_changer.make_change(4)).to eq([1,1,1,1])
+    end
+  end
+
+  context "where I expect a combination of coins" do
+    it "works!" do
+      expect(coin_changer.make_change(87)).to eq([25,25,25,10,1,1])
+    end
+  end
+
+  context "with edge cases" do
+    it "works with zero" do
+      expect(coin_changer.make_change(0)).to eq([])
+    end
+  end
+end
