5fffdac70ca06af7f04c2f464335aaa9c2a4018e
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-21 13:24:55 -0400

    Added a to_s method to Board

--- tictactoe/board.rb
+++ tictactoe/board.rb
@@ -1,16 +1,28 @@
 module TicTacToe
   class Board
-    attr_accessor :cells
+    attr_accessor :data
 
     def initialize(w=3,h=3)
-      @cells = Array.new(w) { Array.new(h) { nil }} # w x h array of arrays
+      @data = Array.new(w) { Array.new(h) { nil }} # w x h array of arrays
     end
 
     def width
-      @cells.length
+      @data.length
     end
     def height
-      @cells[0].length
+      @data[0].length
+    end
+
+    def cells_with_numbers
+      @data.each_with_index.map do |row,i|
+        row.each_with_index.map do |cell,j|
+          cell || i * width + j
+        end
+      end
+    end
+
+    def to_s
+      cells_with_numbers.map{|row| row.join(" | ")}.join("\n---------\n")
     end
   end
 end
--- tictactoe/spec/board_spec.rb
+++ tictactoe/spec/board_spec.rb
@@ -6,13 +6,19 @@ module TicTacToe
       subject(:board) { Board.new }
 
       it "is empty" do
-        expect(board.cells).to eq([[nil,nil,nil],
+        expect(board.data).to eq([[nil,nil,nil],
                                    [nil,nil,nil],
                                    [nil,nil,nil]])
       end
       it "is a 3x3" do
         expect(board.width).to eq(3)
         expect(board.height).to eq(3)
+      end
+
+      describe ".to_s" do
+        it "returns a string representing the board" do
+          expect(board.to_s).to eq("0 | 1 | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8")
+        end
       end
     end
   end
