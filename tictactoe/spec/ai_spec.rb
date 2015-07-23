c7cb8ef46a3013ccf65fa767debc1217214008c0
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-23 12:19:14 -0400

    Added 4x4 test cases

--- tictactoe/spec/ai_spec.rb
+++ tictactoe/spec/ai_spec.rb
@@ -3,7 +3,6 @@ require_relative '../board'
 
 module TicTacToe
 	describe AI do
-		subject(:board) { Board.new }
 		subject(:ai) { AI.new('X','O') }
 
 		it "has token X" do
@@ -14,21 +13,25 @@ module TicTacToe
 		end
 
 		describe ".move" do
+			context "on any size board" do
+				subject(:board) { Board.new }
 
-			context "empty board" do
-				before { ai.move(board) }
-				it "takes the top left" do
-					expect(board.data[0][0]).to eq('X')
+				context "empty board" do
+					before { ai.move(board) }
+					it "takes the top left" do
+						expect(board.data[0][0]).to eq('X')
+					end
 				end
-			end
-			describe "returned value" do
-				it "is the move it makes" do
-					retVal = ai.move(board)
-					expect(retVal).to eq([0,0]) # Assuming the "top left" test passes
+				describe "returned value" do
+					it "is the move it makes" do
+						retVal = ai.move(board)
+						expect(retVal).to eq([0,0]) # Assuming the "top left" test passes
+					end
 				end
 			end
 
 			context "on a 3x3 board" do
+				subject(:board) { Board.new(3,3) }
 				# Win
 				context "two of AI's tokens in a row" do
 					before do
@@ -133,6 +136,88 @@ module TicTacToe
 					end
 					it "wins" do
 						expect(ai.move(board)).to eq([1,2])
+					end
+				end
+			end
+
+			context "on a 4x4 board" do
+				subject(:board) { Board.new(4,4) }
+				# Win
+				context "three of AI's tokens in a row" do
+					before do
+						board.move(0,0,'X')
+						board.move(0,1,'X')
+						board.move(0,2,'X')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([0,3])
+					end
+				end
+				context "three of AI's tokens in a column" do
+					before do
+						board.move(0,0,'X')
+						board.move(1,0,'X')
+						board.move(2,0,'X')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([3,0])
+					end
+				end
+				context "three of AI's tokens in a diagonal" do
+					before do
+						board.move(0,0,'X')
+						board.move(2,2,'X')
+						board.move(3,3,'X')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([1,1])
+					end
+				end
+
+				# Block
+				context "three of AI's tokens in a row" do
+					before do
+						board.move(0,0,'O')
+						board.move(0,1,'O')
+						board.move(0,2,'O')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([0,3])
+					end
+				end
+				context "three of AI's tokens in a column" do
+					before do
+						board.move(0,0,'O')
+						board.move(1,0,'O')
+						board.move(2,0,'O')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([3,0])
+					end
+				end
+				context "three of AI's tokens in a diagonal" do
+					before do
+						board.move(0,0,'O')
+						board.move(2,2,'O')
+						board.move(3,3,'O')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([1,1])
+					end
+				end
+				# Prioritize win over block
+				context "three of Player's tokens in a row, but also three of AI's tokens in a row" do
+					before do
+						board.move(0,0,'O')
+						board.move(0,1,'O')
+						board.move(0,2,'O')
+
+						board.move(1,0,'X')
+						board.move(1,1,'X')
+						board.move(1,2,'X')
+					end
+					it "wins" do
+						expect(ai.move(board)).to eq([1,3])
 					end
 				end
 			end
