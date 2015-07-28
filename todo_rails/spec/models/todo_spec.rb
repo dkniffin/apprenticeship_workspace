16861323876f5bb1da137f6a116452d93ce7acf8
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-28 14:39:53 -0400

    Added tests for Todo's attributes

--- todo_rails/spec/models/todo_spec.rb
+++ todo_rails/spec/models/todo_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:todo) { Todo.new }
+
+  it "has a title" do
+    expect(subject.attribute_names).to include("title")
+  end
+
+  it "has an is_completed flag" do
+    expect(subject.attribute_names).to include("is_completed")
+  end
 end
