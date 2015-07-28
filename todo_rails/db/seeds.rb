9657de1bb4589fa98fe7df4dc0d40165dabe1620
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-28 15:21:05 -0400

    Added seed data

--- todo_rails/db/seeds.rb
+++ todo_rails/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+todos = Todo.create([
+  { title: 'Some Title 1', is_completed: true },
+  { title: 'Some Title 2', is_completed: true },
+  { title: 'Some Title 3', is_completed: false }
+  ])
