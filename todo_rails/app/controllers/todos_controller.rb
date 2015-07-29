b6c3313ecb5a9b4e76a2e333582092579dd135dc
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-29 17:18:31 -0400

    Added POST, GET, and PUT functionality

--- todo_rails/app/controllers/todos_controller.rb
+++ todo_rails/app/controllers/todos_controller.rb
@@ -5,63 +5,26 @@ class TodosController < ApplicationController
     if: Proc.new { |c| c.request.format == 'application/json' }
 
   # GET /todos
-  # GET /todos.json
   def index
-    @todos = Todo.all
-  end
-
-  # GET /todos/1
-  # GET /todos/1.json
-  def show
-  end
-
-  # GET /todos/new
-  def new
-    @todo = Todo.new
-  end
-
-  # GET /todos/1/edit
-  def edit
+    respond_with Todo.all
   end
 
   # POST /todos
-  # POST /todos.json
   def create
-    @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo = Todo.create(todo_params)
+    respond_with @todo
   end
 
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
+  # PATCH/PUT /todos/:id
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo.update(todo_params)
+    respond_with @todo, json: @todo
   end
 
   # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with @todo
   end
 
   private
--- todo_rails/spec/controllers/todos_controller_spec.rb
+++ todo_rails/spec/controllers/todos_controller_spec.rb
@@ -3,20 +3,75 @@ require 'rails_helper'
 RSpec.describe TodosController, type: :controller do
   describe "POST /todos" do
     context "with valid parameters" do
-      subject(:valid_params) { attributes_for(:todo) }
+      let(:valid_params) { attributes_for(:todo) }
 
       it "creates a new todo" do
-        expect{ post :craete, todo: valid_params
+        expect{ post :create, todo: valid_params, format: :json
         }.to change{ Todo.count }.by(1)
       end
-      it "responds with the new todo's info"
+      it "responds with the new todo's info" do
+        post :create, todo: valid_params, format: :json
+        expect(response).to have_http_status(:created)
+        todo_json = response_json["todo"]
+        expect(todo_json["title"]).to eq(valid_params[:title])
+        expect(todo_json["is_completed"]).to eq(valid_params[:is_completed])
+      end
     end
     context "with invalid parameters" do
-      it "doesn't create the new todo"
+      let(:invalid_params) { attributes_for(:todo, title: nil) }
+      it "doesn't create the new todo" do
+        expect{ post :create, todo: invalid_params, format: :json
+        }.to_not change{ Todo.count }
+      end
+      it "responds with an error" do
+        post :create, todo: invalid_params, format: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response_json["errors"]).to include("title")
+        expect(response_json["errors"]["title"]).to include("can't be blank")
+      end
+    end
+  end
+  describe "GET /todos" do
+    let(:todos) { create_list(:todo, 3) }
+
+    it "responds with all the todos" do
+      todos
+      get :index, format: :json
+      expect(response).to have_http_status(:ok)
+      expect(response_json['todos'].length).to eq(3)
+      expect(response_json['todos'].first['title']).to eq(todos.first['title'])
+      expect(response_json['todos'].first['is_completed']).to eq(todos.first['is_completed'])
+    end
+  end
+  describe "PUT /todos/:id" do
+    context "when the id is valid" do
+      context "with valid parameters" do
+        let(:todo) { create(:todo) }
+        let(:valid_params) { attributes_for(:todo, title: "Test title") }
+
+        it "updates the todo" do
+          todo # Create the todo
+          expect { put :update, id: todo.id, todo: valid_params, format: :json
+          }.to change { todo.reload.title }.from(todo.title).to("Test title")
+        end
+        it "responds with the updated todo" do
+          put :update, id: todo.id, todo: valid_params, format: :json
+          expect(response).to have_http_status(:ok)
+          todo_json = response_json["todo"]
+          expect(todo_json["title"]).to eq(valid_params[:title])
+          expect(todo_json["is_completed"]).to eq(valid_params[:is_completed])
+        end
+      end
+    end
+  end
+  describe "DELETE /todos/:id" do
+    context "when the id is valid" do
+      it "deletes the todo"
+      it "sends an empty response"
+    end
+    context "when the id is invalid" do
+      it "doesn't delete any todos"
       it "responds with an error"
     end
   end
-  describe "GET /todos"
-  describe "PUT /todos/3"
-  describe "DELETE /todos/3"
 end
