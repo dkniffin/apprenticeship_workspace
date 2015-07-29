5ff38afc265c72c85e3b13b0878ab59c8547efc5
oddityoverseer13 <derek.kniffin@gmail.com>
2015-07-29 16:24:34 -0400

    Added requirement that todos must have a title

--- todo_rails/spec/models/todo_spec.rb
+++ todo_rails/spec/models/todo_spec.rb
@@ -3,11 +3,25 @@ require 'rails_helper'
 RSpec.describe Todo, type: :model do
   subject(:todo) { Todo.new }
 
-  it "has a title" do
-    expect(subject.attribute_names).to include("title")
-  end
+  it { is_expected.to respond_to(:title)}
+  it { is_expected.to respond_to(:is_completed)}
 
-  it "has an is_completed flag" do
-    expect(subject.attribute_names).to include("is_completed")
+  describe "valid todo" do
+    subject(:todo) { build(:todo) }
+
+    it "is valid" do
+      expect(todo).to be_valid
+    end
+    it "has a title" do
+      expect(todo.title).to be_a(String)
+    end
+  end
+  describe "invalid todo" do
+    let(:no_title) { build(:todo, title: nil) }
+
+    it "receives an error with no title" do
+      expect(no_title).to_not be_valid
+      expect(no_title.errors[:title]).to include("can't be blank")
+    end
   end
 end
